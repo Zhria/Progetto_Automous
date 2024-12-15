36b11d57fb1604667e8a6fa67353ec3b3cbbf304
zakaria <traka.zakaria@gmail.com>
2024-12-15 11:32:03 +0100

    Cambio random

--- rete.py
+++ rete.py
@@ -4,36 +4,33 @@ from keras import layers
 
 #Creiamo una classe che estende tf.keras.Model per definire la nostra rete neurale.
 class ReteNeurale(tf.keras.Model):
-    def __init__(self, dim_in,dim_out,softmax=False):
+    def __init__(self, dim_in,dim_out):
         super(ReteNeurale, self).__init__()
-        self.softmax=softmax
+        self.conv1 = tf.keras.layers.Conv2D(filters=32, kernel_size=(3,3), activation='tanh', input_shape=dim_in)  # Reduced filters, changed activation
         # Layer convoluzionali per estrarre caratteristiche
-        self.conv1 = layers.Conv2D(32, (3, 3), activation='relu', data_format="channels_last", input_shape=dim_in)
-        self.pool1 = layers.MaxPooling2D((2, 2))  # Riduce la dimensione dell'immagine
+        #self.conv1 = layers.Conv2D(32, (3, 3), activation='relu', data_format="channels_last", input_shape=dim_in)
+        #self.pool1 = layers.MaxPooling2D((2, 2))  # Riduce la dimensione dell'immagine
         self.conv2 = layers.Conv2D(64, (3, 3), activation='relu')
-        self.pool2 = layers.MaxPooling2D((2, 2))
-        self.conv3 = tf.keras.layers.Conv2D(128, (3, 3), activation='relu')
-        self.pool3 = tf.keras.layers.MaxPooling2D((2, 2))
+        #self.pool2 = layers.MaxPooling2D((2, 2))
+        self.conv3 = layers.Conv2D(128, (3, 3), activation='relu')
+        #self.pool3 = layers.MaxPooling2D((2, 2))
         self.flatten = layers.Flatten()  # Appiattisce (64, 64, 3) in (64*64*3)
-        self.dense2 = layers.Dense(128, activation='relu')
-        self.dense3 = layers.Dense(dim_out)
+        self.dense2 = layers.Dense(256, activation='relu')
+        self.policy = layers.Dense(dim_out, activation='softmax')
+        self.value= layers.Dense(1)
     def call(self, inputs):
         #Controllo che l'input sia un tensore.
         if not isinstance(inputs, tf.Tensor):
             inputs = tf.convert_to_tensor(inputs)
         x = self.conv1(inputs)
-        x = self.pool1(x)
+        #x = self.pool1(x)
         x = self.conv2(x)
-        x = self.pool2(x)
+        #x = self.pool2(x)
         x = self.conv3(x)
-        x = self.pool3(x)
+        #x = self.pool3(x)
         x = self.flatten(x)        
         x = self.dense2(x)
-        x=self.dense3(x)
-        if(self.softmax):
-            #converto in float64 per evitare problemi di precisione
-            x=tf.dtypes.cast(x,tf.float64)
-            x=tf.nn.softmax(x,axis=1)
-            #Rconverto in float32 per evitare problemi di precisione
-            x=tf.dtypes.cast(x,tf.float32)
-        return x
+        #x=self.dense3(x)
+        policy=self.policy(x)
+        value=self.value(x)
+        return policy,value
